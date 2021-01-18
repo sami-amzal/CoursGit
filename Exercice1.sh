b33aa81b7a34a4960ad74e02113d81e02c2be568
unknown <samiamzal@yahoo.fr>
2021-01-18 12:09:19 +0100

    Exercice 1

---
+++ Exercice1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir MOI
+cd MOI
+mkdir MesDMs
+mkdir MesLettres
+cd MesLettres
+touch Motiv.doc
+touch cv.doc
+cd /mnt/c/Script/MOI
+mkdir MesTps
+cd MesTps
+mkdir TpsUNIX
+mkdir TpsC
+cd TpsC
+mkdir Executables
+touch Tp1.c
+touch Tp2.c
